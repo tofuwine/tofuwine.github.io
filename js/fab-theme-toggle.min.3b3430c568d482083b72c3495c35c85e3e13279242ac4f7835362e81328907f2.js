233abd8282ccc591476c8d7c7344bb7048e93ba0
tofuwine <tofuwine@users.noreply.github.com>
2024-05-15 11:55:31 +0000

    1. 拆分主题内容 2. 更新文章

--- js/fab-theme-toggle.min.3b3430c568d482083b72c3495c35c85e3e13279242ac4f7835362e81328907f2.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("theme-toggle-float").addEventListener("click",()=>{document.body.className.includes("dark")?(document.body.classList.remove("dark"),localStorage.setItem("pref-theme","light")):(document.body.classList.add("dark"),localStorage.setItem("pref-theme","dark"))})
